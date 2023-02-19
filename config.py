3815c6941047c566ec8622328574456416337aeb
KristinaKass <you@example.com5432zx@mail.ru>
2023-02-19 18:41:55 +0700

    Initial Commit

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5807817122:AAFT1AgHDYFBPfu3sZeefb59vXsRK9pk9cY'
+APIKEY = '9d28da8613363bf9a2cb9bd8fc3db0aa6246a560a045adc4f77dd0cf7c3c9e74'
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'юань': 'CNY',
+    'франк': 'CHF',
+}
